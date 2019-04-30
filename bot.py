9e5008d1e5def198987479dfc7df699e819e074b
Justin Chan <justinn.chann@gmail.com>
2019-04-30 18:11:11 -0400

    Ping and choose command

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
 import discord 
 import config
 from discord.ext import commands
+import random
 
-client = discord.Client()
 bot = commands.Bot(command_prefix = "!")
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command()
+async def ping(ctx):
+    await ctx.send('{0} Pong! {1}ms :ping_pong:'.format(ctx.message.author.mention,round(bot.latency*1000)))
+
+@bot.command()
+async def choose(ctx, *choices: str):
+    try:
+        myChoices = " ".join(choices)
+        myChoices = myChoices.split(',')
+        await ctx.send(random.choice(myChoices))
+    except Exception:
+        await ctx.send('Enter at least 1 choice')
         return
-
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
-
-client.run(config.TOKEN)
+        
+bot.run(config.TOKEN)
